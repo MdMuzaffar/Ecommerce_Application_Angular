a5a337fc9fae344ae23b1702d974403cde4f3145
MdMuzaffar <54287360+MdMuzaffar@users.noreply.github.com>
2023-11-17 23:52:46 +0530

    adding component for details view of products

--- ecommerce_application/src/app/app-routing.module.ts
+++ ecommerce_application/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductViewComponent } from './prodect/product-view/product-view.component';
+import { ProductDetailsComponent } from './prodect/product-details/product-details.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component:ProductViewComponent},
+  {path:'product-detail' , component:ProductDetailsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- ecommerce_application/src/app/app.module.ts
+++ ecommerce_application/src/app/app.module.ts
@@ -5,7 +5,7 @@ import {HttpClientModule} from '@angular/common/http' ;
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductViewComponent } from './prodect/product-view/product-view.component';
-import { ProductDetailsComponent } from './prodect/product-view/product-details/product-details.component';
+import { ProductDetailsComponent } from './prodect/product-details/product-details.component';
 
 @NgModule({
   declarations: [
