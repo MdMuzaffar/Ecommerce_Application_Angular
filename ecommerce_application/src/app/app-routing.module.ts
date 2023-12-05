35324cfc8f5003b9fceba437eb7fcbeebb4351e7
MdMuzaffar <54287360+MdMuzaffar@users.noreply.github.com>
2023-12-05 23:10:58 +0530

    Creating cart item page

--- ecommerce_application/src/app/app-routing.module.ts
+++ ecommerce_application/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ProductViewComponent } from './prodect/product-view/product-view.component';
 import { ProductDetailsComponent } from './prodect/product-details/product-details.component';
+import { CartPageComponent } from './prodect/cart-page/cart-page.component';
 
 const routes: Routes = [
   {path:'', component:ProductViewComponent},
-  {path:'product-detail/:productid' , component:ProductDetailsComponent}
+  {path:'product-detail/:productid' , component:ProductDetailsComponent},
+  {path:'cart-item', component:CartPageComponent}
 ];
 
 @NgModule({
